21f6647ff7191a1bfd434af405e4491b9a237fa8
okiktev <oleg.kiktev@gmail.com>
2017-09-26 09:40:26 +0300

    Fixed issue with running from outside

--- build_exe/jarexp.bat
+++ build_exe/jarexp.bat
@@ -2,7 +2,7 @@
 set LOGFILE=%TEMP%\jarexp-console.txt
 set URL=file:///%LOGFILE:\=/%
 echo %date% %time% Starting... >> %LOGFILE%
-call java -jar ".\lib\@FILENAME@" "%~1" >> %LOGFILE% 2>&1
+call java -jar "@FILENAME@" "%~1" >> %LOGFILE% 2>&1
 if %errorlevel% NEQ 0 (
 echo Error code: %errorlevel% >> %LOGFILE%
 mshta "javascript:var sh=new ActiveXObject('WScript.Shell');btn=sh.Popup('Jar Explorer unable to start\nError code: %errorlevel%\nDo you want to see details?',0,'Error',0x4+0x10);switch(btn){case 6:sh.Run('%URL%');break};close()"
